4a67168ae5c037e6e15586c989f7b02e108c0706
YangZhou1994 <zy295479271@163.com>
2017-02-28 16:58:09 +0800

    Fix Errors

--- src/test/java/org/cripac/isee/kafka_test/URL_Test_TopicCreating.java
+++ src/test/java/org/cripac/isee/kafka_test/URL_Test_TopicCreating.java
@@ -8,7 +8,7 @@ import org.cripac.isee.vpe.util.kafka.KafkaHelper;
  * Created by yang on 17-2-27.
  */
 public class URL_Test_TopicCreating {
-    public static void main(){
+    public static void main(String[] arg){
         System.out.println("Connecting to zookeeper: " + "ktask-nod1:2181,ktask-nod2:2181,ktask-nod3:2181,ktask-nod4:2181");
         final ZkUtils zkUtils = KafkaHelper.createZKUtils("ktask-nod1:2181,ktask-nod2:2181,ktask-nod3:2181,ktask-nod4:2181",
                 30000,
